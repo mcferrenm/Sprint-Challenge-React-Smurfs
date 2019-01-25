ded6e40398efc7710a841f4b665cc3e71939ef09
mcferrenm <mcferrenmax@gmail.com>
2019-01-25 15:02:40 -0500

    Final styles

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -18,21 +18,26 @@ const SmurfForm = props => {
           placeholder="name"
           value={props.smurf.name}
           name="name"
+          autoComplete="off"
         />
         <input
           onChange={props.handleInputChange}
           placeholder="age"
           value={props.smurf.age}
           name="age"
+          autoComplete="off"
         />
         <input
           onChange={props.handleInputChange}
           placeholder="height"
           value={props.smurf.height}
           name="height"
+          autoComplete="off"
         />
-        <button type="submit">{props.isUpdating ? "Update" : "Add"}</button>
-        <button onClick={props.cancelEdit}>Cancel</button>
+        <div>
+          <button type="submit">{props.isUpdating ? "Update" : "Add"}</button>
+          <button onClick={props.cancelEdit}>Cancel</button>
+        </div>
       </form>
     </div>
   );
