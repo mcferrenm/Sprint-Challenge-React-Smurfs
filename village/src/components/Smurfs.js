0c3d4c81543fe2df0878c0660dbe2c08f5622b95
mcferrenm <mcferrenmax@gmail.com>
2019-01-25 15:39:46 -0500

    Convert Smurfs component into functinal component

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -3,32 +3,30 @@ import { Link } from "react-router-dom";
 
 import Smurf from "./Smurf";
 
-class Smurfs extends Component {
-  render() {
-    return (
-      <div className="smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Link to={`/smurf/${smurf.id}`} key={smurf.id}>
-                <Smurf
-                  name={smurf.name}
-                  id={smurf.id}
-                  age={smurf.age}
-                  height={smurf.height}
-                  key={smurf.id}
-                  deleteSmurf={this.props.deleteSmurf}
-                  editSmurf={this.props.editSmurf}
-                />
-              </Link>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+const Smurfs = props => {
+  return (
+    <div className="smurfs">
+      <h1>Smurf Village</h1>
+      <ul>
+        {props.smurfs.map(smurf => {
+          return (
+            <Link to={`/smurf/${smurf.id}`} key={smurf.id}>
+              <Smurf
+                name={smurf.name}
+                id={smurf.id}
+                age={smurf.age}
+                height={smurf.height}
+                key={smurf.id}
+                deleteSmurf={props.deleteSmurf}
+                editSmurf={props.editSmurf}
+              />
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 Smurf.defaultProps = {
   smurfs: []
